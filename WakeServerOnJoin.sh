a6bfcf3085d09ff3aa4596e2f0d77d4e31c2ffad
4bitFox <33175205+4bitFox@users.noreply.github.com>
2020-09-26 12:34:01 +0200

    Reduce CPU load

--- WakeServerOnJoin.sh
+++ WakeServerOnJoin.sh
@@ -7,12 +7,17 @@ MAC=XX:XX:XX:XX:XX:XX
 LOG=~/PathToServer/logs/latest.log
  # String that triggers the WoL command. (This should be fine if you use Bungeecord or Waterfall. If you are running a regular server use "joined" or something else instead)
 TRIGGER=connected
+ # How long to wait between each check in seconds.
+T0=0.5
+ # How long to wait after waking real server in seconds.
+T1=300
 
 
 while :
 do
 # Detect if someone joined (or left if you use connected as trigger, but that dun't matter :P).
-if tail -1 $LOG | grep -q $TRIGGER; then
+if tail -1 $LOG | grep -q $TRIGGER
+then
     echo "Sending wake signal to $MAC..." >> $LOG # This line just for preventing grep to detect the previous line in latest-log and spamming the WoL command.
 # Wake server.
     ## If you use wol (Arch Repo)##
@@ -22,6 +27,9 @@ if tail -1 $LOG | grep -q $TRIGGER; then
     ## If you use NetCat ## => https://stackoverflow.com/questions/31588035/bash-one-line-command-to-send-wake-on-lan-magic-packet-without-specific-tool
     #echo -e $(echo $(printf 'f%.0s' {1..12}; printf "$(echo $MAC | sed 's/://g')%.0s" {1..16}) | sed -e 's/../\\x&/g') | nc -w1 -u -b 255.255.255.255 4000
     echo "Sent Wake on Lan signal to $MAC"
+    sleep $T1
+else
+sleep $T0
 fi
 done
 
